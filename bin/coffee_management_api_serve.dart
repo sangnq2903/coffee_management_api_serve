1d9781903d13473dbbfd045d14284d237c4d4a37
sangnq <sangnq@vuthao.com>
2025-06-01 20:56:29 +0700

    first commit

---
+++ bin/coffee_management_api_serve.dart
@@ -0,0 +1,5 @@
+import 'package:coffee_management_api_serve/coffee_management_api_serve.dart' as coffee_management_api_serve;
+
+void main(List<String> arguments) {
+  print('Hello world: ${coffee_management_api_serve.calculate()}!');
+}
---
+++ test/coffee_management_api_serve_test.dart
@@ -0,0 +1,8 @@
+import 'package:coffee_management_api_serve/coffee_management_api_serve.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
